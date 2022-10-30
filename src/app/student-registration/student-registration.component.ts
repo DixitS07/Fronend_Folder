9b9b30fbb324d2efb647d1c31ad42d95ca1380d9
surendra44 <71259552+surendra44@users.noreply.github.com>
2022-10-30 21:25:32 +0530

    middle commit

--- src/app/student-registration/student-registration.component.ts
+++ src/app/student-registration/student-registration.component.ts
@@ -10,14 +10,21 @@ import { AuthService } from '../appServices/auth.service';
 export class StudentRegistrationComponent implements OnInit {
   constructor(private _authservice:AuthService, private _router:Router) { }
   registerdUserData:any={}
+  // selectedFile = null;
+
   ngOnInit(): void {
   }
+  
+  // onFileSelect(event:any){
+  //   this.selectedFile = event.target.files[0]
+  // }
+
   registerUser(){
     // console.log(this.registerdUserData)
     this._authservice.registeredUser(this.registerdUserData).subscribe((res:any)=>{
       console.log(res)
       localStorage.setItem('token', res.token)
-      this._router.navigate(['/special-events'])
+      this._router.navigate(['/special'])
     },
      (err)=>{
       console.log(err)
