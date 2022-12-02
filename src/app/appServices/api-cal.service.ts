46c20c69da3d2f448cd9758eb145e8d2c26b8b07
Dixit <dixitsingh2023@gmail.com>
2022-12-02 18:35:12 +0530

    Done

--- src/app/appServices/api-cal.service.ts
+++ src/app/appServices/api-cal.service.ts
@@ -8,24 +8,34 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 export class ApiCalService {
   badgeCount = new Subject<any>()
   emailR = new Subject<any>()
+  x:string = localStorage.getItem("token")!;
 
-  constructor(private http:HttpClient) {
+  constructor(private http:HttpClient) { 
   }
 
   StudentList():Observable<any>{
+
     console.log(this.badgeCount,'dixit22')
-    var x = localStorage.getItem("token");
-    console.log(x+"dixit33");
-    return this.http.get('http://localhost:3000/api/studentList');
+    // console.log(this.x+"dixit33"); 
+    let headers = new HttpHeaders({
+      'token': this.x
+    })
+    
+    return this.http.get('http://localhost:3000/api/studentList',{headers:headers}
+    );
   }
   
   DeleteRow(id:any):Observable<any>{
+ 
     let params = new HttpParams({
       fromObject: {
         _id:id
       }
     })
-    return this.http.delete('http://localhost:3000/api/student-register',{params:params})
+    let headers = new HttpHeaders({
+      'token': this.x
+    })
+    return this.http.delete('http://localhost:3000/api/student-register',{headers:headers, params:params})
   }
   EditRow(updatedData:any,id:any):Observable<any>{
     // console.log(updatedData,id)
@@ -34,7 +44,10 @@ export class ApiCalService {
         _id:id
       }
     })
-    return this.http.put('http://localhost:3000/api/student-register',updatedData,{params:params})
+    let headers = new HttpHeaders({
+      'token': this.x
+    })
+    return this.http.put('http://localhost:3000/api/student-register',updatedData,{headers:headers, params:params})
   }
   resetPassword(email:any){
     return this.http.post<void>('http://localhost:3000/api/reset-password',email)
